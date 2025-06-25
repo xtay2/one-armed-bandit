bd46858759de3724d06bd5b6a1953a642afa9ff9
xtay2 <dennis_woithe@web.de>
2025-06-25 10:48:52 +0200

    feat(init)

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.xtay2"
+version = "0.0.1"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(24)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/xtay2/onearmedbandit/OneArmedBanditApplication.java
@@ -0,0 +1,13 @@
+package com.xtay2.onearmedbandit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneArmedBanditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneArmedBanditApplication.class, args);
+    }
+
+}
