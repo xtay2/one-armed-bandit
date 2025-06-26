1a9f2369c0a962490dc385939c2b83d1ee1915a2
xtay2 <dennis_woithe@web.de>
2025-06-26 11:20:33 +0200

    feat(database init)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,10 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
